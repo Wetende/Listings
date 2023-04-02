37ca2fe46fd9588fc40945569ca9581752768700
Wetende <cyprianwetende@gmail.com>
2023-04-02 13:13:07 +0300

    media

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import UserProperties
 # Register your models here.
+
+
+
+
+
+class UserPropertiesAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    list_per_page = 25
+
+admin.site.register(UserProperties, UserPropertiesAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from listings.models import Listing
 
 # Create your models here.
+class UserProperties(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def get_listings(self):
+        return Listing.objects.order_by('-list_date').filter(user=self.user)
